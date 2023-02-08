1c95613f0315d1cc5efbd1dee3238d6c37b698c8
kurbatov <kurbatxx@gmail.com>
2023-02-08 13:00:54 +0600

    ws update

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,16 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use futures::{sink::SinkExt, stream::StreamExt};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 use tokio::sync::{mpsc, RwLock};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+static NEXT_USERID: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(1);
+type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Result<Message, axum::Error>>>>>;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct WebDevice {
@@ -23,6 +28,8 @@ struct WebDevice {
 
 #[tokio::main]
 async fn main() {
+    let users = Users::default();
+
     let db_url = dotenvy::var("DATABASE_URL").unwrap();
 
     let pool = PgPoolOptions::new()
@@ -36,7 +43,7 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
         .route("/web_devices", get(web_devices).with_state(pool))
-        .route("/ws", get(handler));
+        .route("/ws", get(handler).with_state(users));
 
     // `POST /users` goes to `create_user`
     //.route("/users", post(create_user));
@@ -70,27 +77,44 @@ async fn web_devices(State(pool): State<Pool<Postgres>>) -> String {
 }
 
 //ws
-async fn handler(ws: WebSocketUpgrade) -> Response {
-    ws.on_upgrade(handle_socket)
+async fn handler(ws: WebSocketUpgrade, State(users): State<Users>) -> Response {
+    //ws.on_upgrade(handle_socket)
+    ws.on_upgrade(move |socket| handle_socket(socket, users))
 }
 
-static NEXT_USERID: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(1);
-
-async fn handle_socket(mut socket: WebSocket) {
+async fn handle_socket(socket: WebSocket, users: Users) {
     let my_id = NEXT_USERID.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
     println!("Welcome User {}", my_id);
 
-    while let Some(msg) = socket.recv().await {
+    let (sender, mut receiver) = socket.split();
+    let (tx, rx) = mpsc::unbounded_channel();
+    let rx = UnboundedReceiverStream::new(rx);
+
+    tokio::spawn(rx.forward(sender));
+    users.write().await.insert(my_id, tx);
+
+    while let Some(msg) = receiver.next().await {
         let msg = if let Ok(msg) = msg {
+            dbg!(&msg);
             format!("{} + text", msg.into_text().unwrap())
         } else {
+            disconnect(my_id, &users).await;
             // client disconnected
             return;
         };
 
-        if socket.send(Message::Text(msg)).await.is_err() {
-            // client disconnected
-            return;
+        for (&_uid, tx) in users.read().await.iter() {
+            if tx.send(Ok(Message::Text(msg.to_string()))).is_err() {
+                // client disconnected
+                disconnect(my_id, &users).await;
+                return;
+            }
         }
     }
+}
+
+async fn disconnect(my_id: usize, users: &Users) {
+    println!("Good bye user {}", my_id);
+
+    users.write().await.remove(&my_id);
 }
