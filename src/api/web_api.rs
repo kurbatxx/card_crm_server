057612d4eb670d11f6f173bbd87f7b05e369ed67
kurbatov <kurbatxx@gmail.com>
2023-02-09 16:07:10 +0600

    login

--- src/api/web_api.rs
+++ src/api/web_api.rs
@@ -1,8 +1,9 @@
-use axum::extract::Query;
+use axum::extract;
 //use once_cell::sync::Lazy;
-use reqwest::{tls, Client};
+use reqwest::{cookie, tls, Client};
 use serde::Deserialize;
 use std::collections::HashMap;
+use tl::NodeHandle;
 
 const SITE_URL: &str = "https://bilim.integro.kz:8181/processor/back-office/index.faces";
 const AUTH_URL: &str = "https://bilim.integro.kz:8181/processor/back-office/j_security_check";
@@ -24,22 +25,38 @@ pub struct AccessData {
     pub password: String,
 }
 
-pub async fn login(Query(query): Query<AccessData>) -> String {
-    let client = create_client_or_send_exist(&query.login);
+pub async fn login(extract::Json(payload): extract::Json<AccessData>) -> String {
+    let client = create_client_or_send_exist(&payload.login);
     let _ = client.get(SITE_URL).send().await.unwrap();
 
     let resp = client
         .post(AUTH_URL)
         .form(&HashMap::from([
-            ("j_username", &query.login),
-            ("j_password", &query.password),
+            ("j_username", &payload.login),
+            ("j_password", &payload.password),
         ]))
         .send()
         .await
         .unwrap();
 
+    let _cookie = resp.cookies().next().unwrap();
+
     let text = resp.text().await.unwrap();
-    text
+
+    //headerForm:sysuser
+    let dom = tl::parse(&text, tl::ParserOptions::default()).unwrap();
+    let parser = dom.parser();
+    let element = dom
+        .get_element_by_id("headerForm:sysuser")
+        .unwrap_or(NodeHandle::new(0))
+        .get(parser)
+        .unwrap();
+
+    if &element.inner_text(parser).to_string() == &payload.login {
+        return "login".to_string();
+    };
+
+    return "error".to_string();
 }
 
 fn create_client_or_send_exist(name: &str) -> Client {
